f0cfc819c71227ae8cbfba88db773c187f5dc24a
Adeyemi Subedat <subedatade@gmail.com>
2023-08-16 10:51:30 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,21 @@
 #include "main.h"
 /**
-* prints 10 times the alphabet, in lowercase, followed by a new line.
+* main - prints 10 times the alphabet, in lowercase, followed by a new line.
 * written by Subedat
 * Return: 0
 */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char ch;
 	int n;
+
 	n = 0;
-	while(n < 10)
+
+	while (n < 10)
 	{
 		ch = 'a';
-		while(ch <= 'z')
+		while (ch <= 'z')
 		{
 			_putchar(ch);
 			ch++;
