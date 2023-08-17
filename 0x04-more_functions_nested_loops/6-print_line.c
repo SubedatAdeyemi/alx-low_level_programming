0be0da3d3b24520712e2f8369798a4f4ce11349f
Adeyemi Subedat <subedatade@gmail.com>
2023-08-17 16:05:52 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  * print_line - draws a straight line in the terminal
- * @n - number of timesthe character _ should be printed
+ * @param n - number of times the character _ should be printed
  */
 
 void print_line(int n)
