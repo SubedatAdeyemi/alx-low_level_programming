a22a3f81137abdd821a012c7fbffa4705a3aa682
Adeyemi Subedat <subedatade@gmail.com>
2023-08-18 16:17:30 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,12 +4,11 @@
  * print_number - prints an integer
  * @n: integer to be printed
  */
-
 void print_number(int n)
 {
 	unsigned int num = n;
 
-	if (n > 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		num = -num;
