90f55533dcf72b24d7e2bfef07079ebe130c4b7b
Adeyemi Subedat <subedatade@gmail.com>
2023-08-24 16:37:00 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,12 +11,12 @@
 void reverse_array(int *a, int n)
 {
 	int i;
-	int j;
+	int t;
 
-	for (i = 0; i < n / 2; i++)
+	for (i = 0; i < n--; i++)
 	{
-		j = a[i];
-		a[i] = a[n - 1 - i];
-		a[n - 1 - i] = j;
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
 	}
 }
