90319ac7f57b94a91d52020ee748fe3c4d845b15
Subedat Adeyemi <subedatade@gmail.com>
2023-11-09 06:56:22 -0800

    Doubly linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ *
+ * @head: head of thelist
+ * Return: no return
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	if  (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+
+	while ((tmp = head) != NULL)
+	{
+		head = head->nextxt;
+		free(tmp);
+	}
+}
